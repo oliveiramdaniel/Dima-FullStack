027dbf0844bd9e102f5744e4a090a4f219d1f9f4
Daniel Oliveira <danielmoliveira@outlook.com>
2024-07-10 22:19:26 -0400

    M09A01

---
+++ Core/Requests/Transactions/CreateTransactionRequest.cs
@@ -0,0 +1,28 @@
+﻿using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Requests.Transactions
+{
+    public class CreateTransactionRequest : Request
+    {
+        [Required(ErrorMessage = "Invalid Title")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Invalid Type")]
+        public ETransactionType Type { get; set; }
+        
+        [Required(ErrorMessage = "Invalid Amount")]
+        public decimal Amount { get; set; }
+        
+        [Required(ErrorMessage = "Invalid Type")]
+        public long CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Invalid Type")]
+        public DateTime? PaidOrReceiveAt { get; set; }
+    }
+}
---
+++ Core/Requests/Transactions/UpdateTransactionRequest.cs
@@ -0,0 +1,30 @@
+﻿using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Requests.Transactions
+{
+    public class UpdateTransactionRequest : Request
+    {
+        public long Id { get; set; }
+        
+        [Required(ErrorMessage = "Invalid Title")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Invalid Type")]
+        public ETransactionType Type { get; set; }
+
+        [Required(ErrorMessage = "Invalid Amount")]
+        public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "Invalid Type")]
+        public long CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Invalid Type")]
+        public DateTime? PaidOrReceiveAt { get; set; }
+    }
+}
