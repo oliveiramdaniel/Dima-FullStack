0da4f2ee3c6988387c68152b4315e5df7938a939
Daniel Oliveira <danielmoliveira@outlook.com>
2024-07-10 22:25:13 -0400

    M09A02

---
+++ Core/Handlers/ITransactionHandler.cs
@@ -0,0 +1,21 @@
+﻿using Core.Requests;
+using Core.Requests.Transactions;
+using Core.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Core.Handlers
+{
+    public interface ITransactionHandler
+    {
+        Task<Response<Transaction>> CreateAsync(CreateTransactionRequest request);
+        Task<Response<Transaction>> UpdateAsync(UpdateTransactionRequest request);
+        Task<Response<Transaction>> DeleteAsync(DeleteTransactionRequest request);
+        Task<Response<Transaction>> GetByIdAsync(GetTransactionByIdRequest request);
+        Task<PagedResponse<List<Transaction>?>> GetByPeriodRequest(GetTransactionByPeriodRequest request);
+    }
+}
