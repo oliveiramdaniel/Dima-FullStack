0f4aa63b917c914cdef43f8d257e57d198cd6cd3
Daniel Oliveira <danielmoliveira@outlook.com>
2024-07-25 22:51:21 -0400

    Finished layouts

--- Web/Program.cs
+++ Web/Program.cs
@@ -6,9 +6,12 @@ using Web;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
 builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
+
 
