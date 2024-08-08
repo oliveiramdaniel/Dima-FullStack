2d7f327342b1371e0f6c7867d62c0a4b25871df3
Daniel Oliveira <danielmoliveira@outlook.com>
2024-08-08 07:56:36 -0400

    M14A19 - Register - Razor

--- Web/Pages/Identity/Register.razor.cs
+++ Web/Pages/Identity/Register.razor.cs
@@ -1,8 +1,8 @@
 ﻿using Core.Handlers;
+using Core.Requests.Account;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor;
-using Web.Security;
 
 namespace Web.Pages.Identity
 {
@@ -13,7 +13,7 @@ namespace Web.Pages.Identity
         public ISnackbar Snackbar { get; set; } = null!;
 
         [Inject]
-        public IAccountHandler AccountHandler { get; set; } = null!;
+        public IAccountHandler Handler { get; set; } = null!;
 
         [Inject]
         public NavigationManager NavigationManager { get; set; } = null!;
@@ -21,5 +21,48 @@ namespace Web.Pages.Identity
         [Inject]
         public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
         #endregion
+
+        #region Properties
+        public bool IsBusy { get; set; } = false;
+        public RegisterRequest InputModel { get; set; } = new RegisterRequest();
+        #endregion
+
+        #region overrides
+        protected override  async Task OnInitializedAsync()
+        {
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+
+            if (user.Identity is {IsAuthenticated: true})
+                NavigationManager.NavigateTo("/");
+        }
+        #endregion
+
+        public async Task OnValidSubmitAsync()
+        {
+            IsBusy = true;
+
+            try
+            {
+                var result = await Handler.RegisterAsync(InputModel);
+
+                if (result.IsSucess)
+                {
+                    Snackbar.Add(result.Message, Severity.Success);
+                    NavigationManager.NavigateTo("/login");
+                }
+                else
+                    Snackbar.Add(result.Message, Severity.Error);
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add(ex.Message, Severity.Error);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+
+        }
     }
 }
